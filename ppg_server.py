bb764fde9f40420e372360318952fc75ea8d4801
avij1109 <avinashjain768@gmail.com>
2025-07-29 11:00:12 +0530

    3

--- ppg_server.py
+++ ppg_server.py
@@ -104,10 +104,7 @@ class PPGProcessor:
                 if elapsed >= 40:
                     self.bp_collection_active = False
                     logger.info("🔵 Auto-stopped BP collection after 40 seconds - analyzing...")
-                    bp_result = self.analyze_bp()
-                    if bp_result:
-                        # Return BP analysis result immediately
-                        return bp_result
+                    return self.analyze_bp()
             
             # Always return PPG signal data immediately
             base_result = {
